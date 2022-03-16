1c436985e213e7f4e952b10d3ff581235d7fe944
Didrik Lindberg Roest <didrikr@viken.no>
2022-03-16 15:34:00 +0100

    Vin

--- haralds_vintrekking/script.js
+++ haralds_vintrekking/script.js
@@ -1,53 +1,123 @@
+// Lager variabler basert på DOM
 let minEl = document.querySelector('#min')
 let maxEl = document.querySelector('#max')
-
 let topEl = document.querySelector('#top')
-
 let btnEl = document.querySelector("#btn")
+let HaraldEl = document.querySelector('.Harald')
 
-let imgEl = document.querySelector('.Harald')
+// Lager et array med heltall fra og med low til og med high
+function nArray(low, high){
+    let i = low;
+    arr = [i]
+    while (i < high){
+        i++;
+        arr.push(i)
+    }
+
+    return arr
+}
+
+// Ansatt klasse
+class Ansatt{
+    constructor(name, low, high, hasImage=false){
+        this.name = name
+        this.numbers = nArray(low, high)
+        this.hasImage = hasImage
+    }
+}
 
 
-window.addEventListener('resize', () => {
+// Lager objekter av de ansatte, og legger i et array 
+let ansatte = []
+ansatte.push(new Ansatt("Asbjørn", 1, 12))
+ansatte.push(new Ansatt("Tore", 13, 24))
+ansatte.push(new Ansatt("Mari K. K.", 25, 30))
+ansatte.push(new Ansatt("Kirsti", 31, 36))
+ansatte.push(new Ansatt("Mari K. K.", 37, 42))
+ansatte.push(new Ansatt("Johanne", 43, 47))
+ansatte.push(new Ansatt("Kristin", 48, 59))
+ansatte.push(new Ansatt("Kirsti", 60, 65))
+ansatte.push(new Ansatt("Johanne", 66, 69))
+ansatte.push(new Ansatt("Thomas R.", 70, 81))
+ansatte.push(new Ansatt("Johanne", 82, 84))
+ansatte.push(new Ansatt("Harald", 85, 96, true))
+ansatte.push(new Ansatt("Otto A.", 97, 108))
+ansatte.push(new Ansatt("Anders H.", 109, 120, true))
+ansatte.push(new Ansatt("Mira", 121, 132))
+ansatte.push(new Ansatt("Turid", 133, 144))
+ansatte.push(new Ansatt("Vegard", 145, 156))
+ansatte.push(new Ansatt("Didrik", 157, 161, true))
+
+
+
+window.addEventListener('resize', setHarald);
+window.addEventListener('load', setHarald);
+
+function setHarald(){
     if (window.innerWidth <= 650) {
-        imgEl.src = "Harald.jpg"
+        /* Bildet er hentet fra Viken fylkeskommune */
+        HaraldEl.src = "./bilder./ansatte/Harald.jpg"
     }
     else {
-        imgEl.src = "Harald_bibliotek.jpg"
+        /* Bilde er hentet fra: https://www.budstikka.no/debatt/la-biblioteket-leve/25958!/ */
+        HaraldEl.src = "./bilder/Harald_bibliotek.jpg"
     }
-});
+}
 
 
 
 topNumberEl = document.querySelector('#topNumber')
 
 let audio = new Audio('drumroll_tada.mp3');
+audio.load()
 
 function tilfeldigTall(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function tilfeldigTrekk(){
+    audio.load()
     let min = Number(minEl.value)
     let max = Number(maxEl.value)
 
     btnEl.classList.toggle("trukket")
-
+    
     if (btnEl.classList.contains("trukket")) {
-        let tilfeldig = Math.floor(Math.random() * (max - min + 1)) + min
-
         audio.play();
-        imgEl.classList.toggle("zoom")
-        imgEl.style.height = '0';
-        
+        let tilfeldig = tilfeldigTall(min, max)
+        let navn = "";
+        let harBilde = false;
+        ansatte.forEach(ansatt => {
+            if(ansatt.numbers.includes(tilfeldig)){
+                navn = ansatt.name
+                harBilde = ansatt.hasImage
+            }
+        })
 
-        topNumberEl.innerText = tilfeldig
+        HaraldEl.classList.toggle("zoom")
+        HaraldEl.style.height = '0';
+        
+        topNumberEl.innerHTML = `<p>${tilfeldig}</p>`
+
+        if (navn != ""){
+            topNumberEl.innerHTML += `<p>(${navn})</p>`
+
+            if(harBilde){
+                topNumberEl.innerHTML += `<img src="./bilder/ansatte/${navn}.jpg" alt="${navn}">`
+            }
+
+            topNumberEl.style.fontSize = "60px"            
+        }
+        else {
+            topNumberEl.style.fontSize = "120px" 
+        }
+
         btnEl.innerText = "Igjen?"
     } else {
         btnEl.innerText = "Trekk!"
         
-        imgEl.classList.toggle("zoom")
-        imgEl.style.height = '90%'
+        HaraldEl.classList.toggle("zoom")
+        HaraldEl.style.height = '95%'
         topNumberEl.innerText = "0"
     }
     
