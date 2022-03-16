41bbbf154cf280cec0b1582da1d43108e26a5348
Didrik Lindberg Roest <didrikr@viken.no>
2022-03-16 12:02:28 +0100

    t

---
+++ terningspill/script.js
@@ -0,0 +1,89 @@
+let terningerEl = document.querySelector(".terninger");
+let remaining = 4;
+
+
+function terning(){
+    return Math.floor(Math.random()*6) + 1;
+}
+
+let k;
+
+function addClickEvent(){
+    let terningAll = document.querySelectorAll(".terning");
+
+    terningAll.forEach(terning => {
+        terning.addEventListener('click', (e) => {
+            terning.classList.toggle('chosen');
+        })
+    })
+}
+
+function rollAllDice(){
+    terningerEl.innerHTML = "";
+    for (let i=1; i<=5; i++){
+        k = terning()
+        terningerEl.innerHTML += `
+            <div class="terning" id="terning${k}">
+                <img src="./figurer/dice${k}.png" id="${k}">
+            </div>
+        `
+    }
+
+    addClickEvent();
+    calculateSum();
+}
+
+//rollAllDice();
+
+
+function rollNotChosenDice(){
+    let terningAll = document.querySelectorAll(".terning");
+    let n = 5;
+    terningerEl.innerHTML = "";
+
+    terningAll.forEach(terning => {
+        if(terning.classList.contains('chosen')){
+            n--;
+            terningerEl.innerHTML += terning.outerHTML
+        }
+    })
+
+    for (let i=1; i<=n; i++){
+        k = terning()
+        terningerEl.innerHTML += `
+            <div class="terning" id="terning${k}">
+                <img src="./figurer/dice${k}.png" id="${k}">
+            </div>
+        `
+    }
+
+    addClickEvent();
+    calculateSum();
+}
+
+function calculateSum(){
+    let sum = 0;
+    let terningBildeAll = document.querySelectorAll("img");
+
+    terningBildeAll.forEach(terningBilde => {
+        sum += Number(terningBilde.id);
+    })
+
+    let h1El = document.querySelector("#score");
+
+    h1El.innerText = `Score: ${sum}`
+
+    let h2El = document.querySelector("#remaining");
+    remaining--;
+
+    h2El.innerText = `Du har ${remaining} kast igjen`
+
+    if (remaining <= 0){
+        document.getElementById("knappAlle").style.display = "none";
+        document.getElementById("knappValgt").style.display = "none";
+    }
+}
+
+
+// Kaster alle terningene når nettsiden laster inn
+rollAllDice()
