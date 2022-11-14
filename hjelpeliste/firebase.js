aa6f02666aca0c93bbdc40a45c41bf0d286b66ec
Didrik Roest <didrik.roest@yahoo.com>
2022-11-14 19:26:48 +0100

    New database

--- hjelpeliste/firebase.js
+++ hjelpeliste/firebase.js
@@ -1,12 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBNg4iowN0vaGUOQm8wOJnmqWswrVqzEuc",
-    authDomain: "blokkd.firebaseapp.com",
-    projectId: "blokkd",
-    storageBucket: "blokkd.appspot.com",
-    messagingSenderId: "777073264345",
-    appId: "1:777073264345:web:b764cac5bd8c66f4d6a0c8"
+    apiKey: "AIzaSyCnMXmPMWYGB-1Cyjf9fGxCSdv255ikkYc",
+    authDomain: "learning-147.firebaseapp.com",
+    projectId: "learning-147",
+    storageBucket: "learning-147.appspot.com",
+    messagingSenderId: "367100846840",
+    appId: "1:367100846840:web:c5a31e9a78350dcd8b910d",
+    measurementId: "G-CCRMJ16XJS"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
