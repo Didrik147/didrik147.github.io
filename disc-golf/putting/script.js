76bb523993b0a8bd3e8a83f14db62cd702e29bef
Didrik147 <didrik.roest@yahoo.com>
2024-01-12 21:56:26 +0100

    It works

--- disc-golf/putting/script.js
+++ disc-golf/putting/script.js
@@ -4,12 +4,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrjzn3bcOod4roVQGcbzyOMC3VQmIdxP8",
-    authDomain: "hobbies-147.firebaseapp.com",
-    projectId: "hobbies-147",
-    storageBucket: "hobbies-147.appspot.com",
-    messagingSenderId: "751737489182",
-    appId: "1:751737489182:web:e2a011e600b257ce8deb0f"
+  apiKey: "AIzaSyCrjzn3bcOod4roVQGcbzyOMC3VQmIdxP8",
+  authDomain: "hobbies-147.firebaseapp.com",
+  projectId: "hobbies-147",
+  storageBucket: "hobbies-147.appspot.com",
+  messagingSenderId: "751737489182",
+  appId: "1:751737489182:web:e2a011e600b257ce8deb0f"
 };
 
 
@@ -40,6 +40,8 @@ const usernameEl = document.querySelector("#username")
 const startBtn = document.querySelector("#start")
 const submitBtn = document.querySelector("#submit")
 
+const bodyEl = document.querySelector('body')
+
 let distance_from;
 let distance_to;
 let nPutters;
@@ -47,29 +49,29 @@ let C1X_percentage;
 
 let moldArr = []
 
-for(let i=0; i<moldSelectEl.children.length; i++){
-    moldArr.push(moldSelectEl.children[i].value)
-    if (moldSelectEl.children[i].value == localStorage.mold){
-        moldSelectEl.children[i].selected = true
-    }
+for (let i = 0; i < moldSelectEl.children.length; i++) {
+  moldArr.push(moldSelectEl.children[i].value)
+  if (moldSelectEl.children[i].value == localStorage.mold) {
+    moldSelectEl.children[i].selected = true
+  }
 }
 
 let putterName = moldSelectEl.value
 
-if(localStorage.putters){
-    putterInputEl.placeholder = localStorage.putters
+if (localStorage.putters) {
+  putterInputEl.placeholder = localStorage.putters
 }
 
-if(localStorage.fromDistance){
-    fromInputEl.placeholder = localStorage.fromDistance
+if (localStorage.fromDistance) {
+  fromInputEl.placeholder = localStorage.fromDistance
 }
 
-if(localStorage.toDistance){
-    toInputEl.placeholder = localStorage.toDistance
+if (localStorage.toDistance) {
+  toInputEl.placeholder = localStorage.toDistance
 }
 
-if(localStorage.username){
-    usernameEl.value = localStorage.username
+if (localStorage.username) {
+  usernameEl.value = localStorage.username
 }
 
 
@@ -78,263 +80,178 @@ putterInputEl.addEventListener('input', updatePuttersLS)
 fromInputEl.addEventListener('input', updateFromDistanceLS)
 toInputEl.addEventListener('input', updateToDistanceLS)
 
-function updateMoldLS(){
-    console.log("Updating mold in local storage")
-    localStorage.mold = moldSelectEl.value
+function updateMoldLS() {
+  console.log("Updating mold in local storage")
+  localStorage.mold = moldSelectEl.value
 }
 
-function updatePuttersLS(){
-    console.log("Updating number of putters in local storage")
-    localStorage.putters = putterInputEl.value
+function updatePuttersLS() {
+  console.log("Updating number of putters in local storage")
+  localStorage.putters = putterInputEl.value
 }
 
-function updateFromDistanceLS(){
-    console.log("Updating distance in local storage")
-    localStorage.fromDistance = fromInputEl.value
+function updateFromDistanceLS() {
+  console.log("Updating distance in local storage")
+  localStorage.fromDistance = fromInputEl.value
 }
 
-function updateToDistanceLS(){
-    console.log("Updating distance in local storage")
-    localStorage.toDistance = toInputEl.value
+function updateToDistanceLS() {
+  console.log("Updating distance in local storage")
+  localStorage.toDistance = toInputEl.value
 }
 
 
 
 
 function updateMain() {
-    //console.log("Updating HTML") 
-    mainEl.innerHTML = ""
-    if (fromInputEl.value == '') {
-        distance_from = Number(fromInputEl.placeholder)
-    } else {
-        distance_from = Number(fromInputEl.value)
-    }
+  //console.log("Updating HTML") 
+  mainEl.innerHTML = ""
+  if (fromInputEl.value == '') {
+    distance_from = Number(fromInputEl.placeholder)
+  } else {
+    distance_from = Number(fromInputEl.value)
+  }
 
-    if (toInputEl.value == '') {
-        distance_to = Number(toInputEl.placeholder)
-    } else {
-        distance_to = Number(toInputEl.value)
-    }
+  if (toInputEl.value == '') {
+    distance_to = Number(toInputEl.placeholder)
+  } else {
+    distance_to = Number(toInputEl.value)
+  }
 
 
 
-    for (let d = distance_from; d <= distance_to; d++) {
-        mainEl.innerHTML += `
+  for (let d = distance_from; d <= distance_to; d++) {
+    mainEl.innerHTML += `
         <article>
             <h2>${d} meter</h2>
             <div class="buttonrow row${d}">
             </div>
         </article>
     `
-        const buttonrowEl = document.querySelector(`.row${d}`)
-        if (putterInputEl.value == '') {
-            nPutters = Number(putterInputEl.placeholder)
-        } else {
-            nPutters = Number(putterInputEl.value)
-        }
+    const buttonrowEl = document.querySelector(`.row${d}`)
+    if (putterInputEl.value == '') {
+      nPutters = Number(putterInputEl.placeholder)
+    } else {
+      nPutters = Number(putterInputEl.value)
+    }
 
-        for (let i = 0; i <= nPutters; i++) {
-            buttonrowEl.innerHTML += `
+    for (let i = 0; i <= nPutters; i++) {
+      buttonrowEl.innerHTML += `
             <button>${i}</button>
         `
-        }
-
-        let gap = 30 / (nPutters * 2)
-        buttonrowEl.style.gap = `${gap}%`
-
-
-        updateButtons()
     }
+
+    let gap = 30 / (nPutters * 2)
+    buttonrowEl.style.gap = `${gap}%`
+
+
+    updateButtons()
+  }
 }
 
 function updateButtons() {
-    const buttonEls = document.querySelectorAll("main button")
+  const buttonEls = document.querySelectorAll("main button")
 
-    buttonEls.forEach(buttonEl => {
-        buttonEl.addEventListener("click", clickButton)
-    })
+  buttonEls.forEach(buttonEl => {
+    buttonEl.addEventListener("click", clickButton)
+  })
 
-    /* Making it similar to radio button */
-    function clickButton(e) {
-        if (e.target.classList.contains("clicked")) {
-            e.target.classList.remove("clicked")
-        } else {
-            let row = e.target.parentElement.classList[1]
+  /* Making it similar to radio button */
+  function clickButton(e) {
+    if (e.target.classList.contains("clicked")) {
+      e.target.classList.remove("clicked")
+    } else {
+      let row = e.target.parentElement.classList[1]
 
-            const buttonsInRow = document.querySelectorAll(`.${row} > button`)
+      const buttonsInRow = document.querySelectorAll(`.${row} > button`)
 
-            buttonsInRow.forEach(buttonEl => {
-                buttonEl.classList.remove("clicked")
-            })
-            e.target.classList.add("clicked")
-        }
+      buttonsInRow.forEach(buttonEl => {
+        buttonEl.classList.remove("clicked")
+      })
+      e.target.classList.add("clicked")
     }
+  }
 }
 
 /* Database */
 let collectionName;
 
-let addData = {
-    '4m' : [0, 0],
-    '5m' : [0, 0],
-    '6m' : [0, 0],
-    '7m' : [0, 0],
-    '8m' : [0, 0],
-    '9m' : [0, 0],
-}
-
-let newData = {
-    ...addData
-}
-let oldData = {
-    ...addData
-}
-
-let distanceArr = ['4m', '5m', '6m', '7m', '8m', '9m']
-
-/* Resetting data for all molds (CAREFUL) */
-/* moldArr.forEach(mold => {
-    resetMoldData(mold)
-}) */
+//let distanceArr = ['4m', '5m', '6m', '7m', '8m', '9m']
 
 
 /* Starting the putting practice */
 startBtn.addEventListener("click", startPractice)
 
+
 async function startPractice() {
-    // Get putter name
-    putterName = moldSelectEl.value
-    localStorage.username = usernameEl.value
-    collectionName = localStorage.username
-    //collectionName = 'putting'
-
-    //console.log(putterName)
-
-    // Get previous data from database
-    await getPutterData(putterName)
-
-    // Remove background
-    document.querySelector('body').style.background='none'
-    headerEl.style.margin='0px'
-
-    console.log(`Putter: ${putterName}`)
-    console.log(`Data before this putting practice (made/tried)`)
-    
-    distanceArr.forEach(d => {
-        console.log(`${d}: ${oldData[d][0]}/${oldData[d][1]}`)
-    })
+  // Get putter name
+  putterName = moldSelectEl.value
+  localStorage.username = usernameEl.value
+  collectionName = localStorage.username
+  //collectionName = 'new_test'
 
 
-    headerEl.innerHTML = `
+  // Remove background
+  bodyEl.style.background = 'none'
+  headerEl.style.margin = '0px'
+
+  //console.log(`Putter: ${putterName}`)
+
+
+  headerEl.innerHTML = `
     <h1>Putter: ${putterName}</h1>
     <p>For each distance, press the button that corresponds to the number of putters that went in. You can skip a distance by leaving it blank.</p>
     `
 
-    // Play sound
-    //document.getElementById('pottis').play();
+  // Play sound
+  //document.getElementById('pottis').play();
 
-    updateMain()
-    startBtn.classList.toggle('hide')
-    submitBtn.classList.toggle('hide')
-}
-
-
-async function getPutterData(putterName) {
-    // Get discs from database
-    const q = query(collection(db, collectionName))
-    const querySnapshot = await getDocs(q)
-
-    querySnapshot.forEach((doc) => {
-        //console.log(doc)
-        //console.log(doc.id, " => ", doc.data())
-
-        if (doc.id == putterName){
-            let disc = doc.data()
-            distanceArr.forEach(dist => {
-                oldData[dist] = disc[dist]
-            })
-            
-        }
-    })
-}
-
-async function resetMoldData(mold){
-    console.log("Adding disc to database")
-
-    // Add a new document with a specified ID
-    await setDoc(doc(db, collectionName, mold), {
-        name: mold,
-        '4m': [0, 0],
-        '5m': [0, 0],
-        '6m': [0, 0],
-        '7m': [0, 0],
-        '8m': [0, 0],
-        '9m': [0, 0]
-    })
-}
-
-
-
-
-async function updatePutter(newData) {
-    console.log("Updating database")
-
-    // Add a new document with a specified ID
-    await setDoc(doc(db, collectionName, putterName), {
-        name: putterName,
-        '4m': newData['4m'], 
-        '5m': newData['5m'], 
-        '6m': newData['6m'], 
-        '7m': newData['7m'], 
-        '8m': newData['8m'], 
-        '9m': newData['9m'], 
-    })
+  updateMain()
+  startBtn.classList.toggle('hide')
+  submitBtn.classList.toggle('hide')
 }
 
 
 submitBtn.addEventListener('click', submitData)
 
+
 function submitData() {
-    console.log('Logging results')
 
-    let C1X_made = 0
-    let C1X_tried = 0
+  let C1X_made = 0
+  let C1X_tried = 0
 
-    
-    for (let d = distance_from; d <= distance_to; d++) {
-        let nSuccess = -1
-        const buttonsInRow = document.querySelectorAll(`.row${d} > button`)
-        buttonsInRow.forEach(buttonEl => {
-            if (buttonEl.classList.contains('clicked')) {
-                nSuccess = Number(buttonEl.innerHTML)
-            }
-        })
-        if (nSuccess >= 0) {
-            addData[`${d}m`] = [nSuccess, nPutters]
+  let dataObj = { "name": putterName }
 
-            if (d > 3.3 && d < 10) {
-                C1X_made += nSuccess
-                C1X_tried += nPutters
-            }
-            console.log(`${d} m: ${nSuccess}/${nPutters}`)
-        }
-    }
 
-    console.log(addData)
-
-    distanceArr.forEach(dist => {
-        newData[dist] = [
-            oldData[dist][0] + addData[dist][0],
-            oldData[dist][1] + addData[dist][1],
-        ]
+  for (let d = distance_from; d <= distance_to; d++) {
+    let nSuccess = -1
+    const buttonsInRow = document.querySelectorAll(`.row${d} > button`)
+    buttonsInRow.forEach(buttonEl => {
+      if (buttonEl.classList.contains('clicked')) {
+        nSuccess = Number(buttonEl.innerHTML)
+      }
     })
+    if (nSuccess >= 0) {
+      dataObj[`${d}m`] = [nSuccess, nPutters]
 
-    updatePutter(newData)
+      if (d > 3.3 && d < 10) {
+        C1X_made += nSuccess
+        C1X_tried += nPutters
+      }
+      console.log(`${d} m: ${nSuccess}/${nPutters}`)
+    }
+  }
+
+  if (Object.keys(dataObj).length > 1) {
+    console.log('Logging results')
+    console.log(dataObj)
+
+    updateDB(dataObj)
 
     C1X_percentage = 100 * (C1X_made / C1X_tried)
     C1X_percentage = Math.round(C1X_percentage * 10) / 10
 
-    
+
     headerEl.innerHTML = `<h1>Putter: ${putterName}</h1>`
     mainEl.innerHTML = '<h2 class="center">Data sent</h2>'
     mainEl.innerHTML += `<h3 class="center">C1X percentage: ${C1X_percentage} %</h3>`
@@ -347,67 +264,80 @@ function submitData() {
     /* footerEl.innerHTML = '<button onclick="location.reload()">Try again?</button>' */
 
     //console.log(data)
+  }else {
+    alert('No data to be stored')
+  }
 }
 
+
+async function updateDB(data) {
+  console.log("Updating database")
+
+  let timestamp = new Date().getTime().toString()
+  console.log("Timestamp:", timestamp)
+
+  // Add a new document with a specified ID
+  await setDoc(doc(db, collectionName, timestamp), data)
+}
 
 
 // Function for drawing pie chart
 function drawPieChart(p) {
-    document.querySelector('.chart-container').classList.remove('hide')
+  document.querySelector('.chart-container').classList.remove('hide')
 
-    let ctx = document.getElementById('pieChart').getContext('2d');
+  let ctx = document.getElementById('pieChart').getContext('2d');
 
-    let pieChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            datasets: [{
-                data: [p, 100-p],
-                backgroundColor: ['#43AA8B', '#FB3640']
-            }],
-            labels: ['Putt made', 'Putts missed']
+  let pieChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      datasets: [{
+        data: [p, 100 - p],
+        backgroundColor: ['#43AA8B', '#FB3640']
+      }],
+      labels: ['Putt made', 'Putts missed']
+    },
+    options: {
+      responsive: true,
+      /* rotation: 45, */
+      legend: {
+        position: 'bottom',
+        onClick: null,
+        reverse: true,
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: 'Custom Chart Title'
         },
-        options: {
-            responsive: true,
-            /* rotation: 45, */
-            legend: {
-                position: 'bottom',
-                onClick: null,
-                reverse: true,
-            },
-            plugins: {
-                title: {
-                    display: true,
-                    text: 'Custom Chart Title'
-                },
-                labels: {
-                    // render 'label', 'value', 'percentage', 'image' or custom function, default is 'percentage'
-                    render: 'percentage',
+        labels: {
+          // render 'label', 'value', 'percentage', 'image' or custom function, default is 'percentage'
+          render: 'percentage',
 
-                    // identifies whether or not labels of value 0 are displayed, default is false
-                    showZero: true,
+          // identifies whether or not labels of value 0 are displayed, default is false
+          showZero: true,
 
-                    // font size, default is defaultFontSize
-                    fontSize: 18,
+          // font size, default is defaultFontSize
+          fontSize: 18,
 
-                    // font color, can be color array for each data or function for dynamic color, default is defaultFontColor
-                    fontColor: '#fff',
+          // font color, can be color array for each data or function for dynamic color, default is defaultFontColor
+          fontColor: '#fff',
 
-                    // draw text shadows under labels, default is false
-                    textShadow: true,
+          // draw text shadows under labels, default is false
+          textShadow: true,
 
-                    // text shadow intensity, default is 6
-                    shadowBlur: 10,
+          // text shadow intensity, default is 6
+          shadowBlur: 10,
 
-                    // text shadow X offset, default is 3
-                    shadowOffsetX: 0,
+          // text shadow X offset, default is 3
+          shadowOffsetX: 0,
 
-                    // text shadow Y offset, default is 3
-                    shadowOffsetY: 0,
+          // text shadow Y offset, default is 3
+          shadowOffsetY: 0,
 
-                    // text shadow color, default is 'rgba(0,0,0,0.3)'
-                    shadowColor: 'rgba(0,0,0,1)',
-                }
-            }
+          // text shadow color, default is 'rgba(0,0,0,0.3)'
+          shadowColor: 'rgba(0,0,0,1)',
         }
-    })
+      }
+    }
+  })
 }
--- disc-golf/putting/summary.js
+++ disc-golf/putting/summary.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrjzn3bcOod4roVQGcbzyOMC3VQmIdxP8",
-    authDomain: "hobbies-147.firebaseapp.com",
-    projectId: "hobbies-147",
-    storageBucket: "hobbies-147.appspot.com",
-    messagingSenderId: "751737489182",
-    appId: "1:751737489182:web:e2a011e600b257ce8deb0f"
+  apiKey: "AIzaSyCrjzn3bcOod4roVQGcbzyOMC3VQmIdxP8",
+  authDomain: "hobbies-147.firebaseapp.com",
+  projectId: "hobbies-147",
+  storageBucket: "hobbies-147.appspot.com",
+  messagingSenderId: "751737489182",
+  appId: "1:751737489182:web:e2a011e600b257ce8deb0f"
 };
 
 
@@ -24,171 +24,195 @@ const db = getFirestore(app);
 
 // Elements from DOM
 const userEl = document.querySelector('#user')
-const mainEl = document.querySelector('main')
 const tableContainerEl = document.querySelector('#table-container')
 const c1xEl = document.querySelector('#c1x')
-const bodyEl = document.querySelector('body')
+const inputEl = document.querySelector('#input')
+const button = document.querySelector('button')
 
 let username = userEl.value
 
 let distances = ["4m", "5m", "6m", "7m", "8m", "9m"]
 
-let C1X_data = {
+userEl.addEventListener('change', (e) => {
+  inputEl.value = e.target.value
+})
+
+button.addEventListener('click', (e) => {
+  let username = inputEl.value
+
+  if (username.length > 0){
+    getUserData(username)
+  }
+})
+
+
+window.addEventListener("load", (e) => {
+  if(localStorage.username){
+    inputEl.value = localStorage.username
+  }else{
+    inputEl.value = userEl.value
+  }
+})
+
+
+async function getUserData(username) {
+  let discData = {}
+
+  // Get information about user from database
+  const q = query(collection(db, username))
+  const querySnapshot = await getDocs(q)
+  if (querySnapshot.size == 0) {
+    alert("User has no data")
+  } else {
+    querySnapshot.forEach((doc) => {
+      let practice = doc.data()
+      let mold = practice.name
+
+      if (!(mold in discData)) {
+        discData[mold] = {
+          '4m': [0, 0],
+          '5m': [0, 0],
+          '6m': [0, 0],
+          '7m': [0, 0],
+          '8m': [0, 0],
+          '9m': [0, 0],
+        }
+      }
+
+      distances.forEach(distance => {
+        if (distance in practice) {
+          discData[mold][distance][0] += practice[distance][0]
+          discData[mold][distance][1] += practice[distance][1]
+        }
+      })
+    })
+
+    console.log(discData)
+    createTable(discData)
+  }
+}
+
+
+
+function createTable(discData) {
+  let C1X_data = {
     '4m': [0, 0],
     '5m': [0, 0],
     '6m': [0, 0],
     '7m': [0, 0],
     '8m': [0, 0],
     '9m': [0, 0],
-}
+  }
 
+  tableContainerEl.innerHTML = `<h2 class="center">Data</h2>`
 
+  // Lager et tabellelement
+  let tableEl = document.createElement('table')
 
-userEl.addEventListener('change', (e) => {
-    username = e.target.value
-    /* if (username.length > 0) {
-        getUserData(username)
-    } */
-    getUser(username)
-})
+  // Lager thead
+  let theadEl = document.createElement('thead')
 
-async function getUser(username) {
-    // Get information about user from database
-    const q = query(collection(db, username))
-    const querySnapshot = await getDocs(q)
+  // Lager tr for thead
+  let trEl = document.createElement('tr')
 
-    let discs = []
+  let headerArr = ["Mold", "4 m", "5 m", "6 m", "7 m", "8 m", "9 m", "C1X"]
 
-    if (querySnapshot.size == 0) {
-        console.log("User has no data")
-    } else {
-        querySnapshot.forEach((doc) => {
-            discs.push(doc.data())
-        })
-    }
-    getData(discs)
-}
+  headerArr.forEach(head => {
+    let thEl = document.createElement('th')
+    thEl.innerText = head
+    trEl.appendChild(thEl)
+  })
 
+  // Legger raden til thead
+  theadEl.appendChild(trEl)
 
-getUser(username)
+  // Legger thead til table
+  tableEl.appendChild(theadEl)
 
-function getData(discs) {
-    C1X_data = {
-        '4m': [0, 0],
-        '5m': [0, 0],
-        '6m': [0, 0],
-        '7m': [0, 0],
-        '8m': [0, 0],
-        '9m': [0, 0],
-    }
-    
-    tableContainerEl.innerHTML = `<h2 class="center">Data</h2>`
+  // Lager tbody element
+  let tbodyEl = document.createElement('tbody')
 
-    // Lager et tabellelement
-    let tableEl = document.createElement('table')
+  let madeTotal = 0
+  let triedTotal = 0
 
-    // Lager thead
-    let theadEl = document.createElement('thead')
+  // Går gjennom data for alle de ulike putterne
+  let discs = Object.keys(discData)
 
-    // Lager tr for thead
+  discs.forEach(disc => {
     let trEl = document.createElement('tr')
 
-    let headerArr = ["Mold", "4 m", "5 m", "6 m", "7 m", "8 m", "9 m", "C1X"]
+    // Navnet på discen
+    let tdEl = document.createElement('td')
+    tdEl.innerHTML = disc
 
-    headerArr.forEach(head => {
-        let thEl = document.createElement('th')
-        thEl.innerText = head
-        trEl.appendChild(thEl)
+    // Legger kolonnen til raden
+    trEl.appendChild(tdEl)
+
+    let made = 0
+    let tried = 0
+
+    // Lager kolonner
+    // Går gjennom alle distansene
+    distances.forEach(distance => {
+      // Putting stats
+      tdEl = document.createElement('td')
+      tdEl.innerHTML = `${discData[disc][distance][0]} / ${discData[disc][distance][1]}`
+
+      made += discData[disc][distance][0]
+      tried += discData[disc][distance][1]
+
+      // Legger kolonnen til raden
+      trEl.appendChild(tdEl)
+
+      C1X_data[distance][0] += discData[disc][distance][0]
+      C1X_data[distance][1] += discData[disc][distance][1]
     })
 
-    // Legger raden til thead
-    theadEl.appendChild(trEl)
+    let C1X_percentage = (made / tried) * 100
+    C1X_percentage = Math.round(C1X_percentage)
 
-    // Legger thead til table
-    tableEl.appendChild(theadEl)
+    // Siste kolonne (C1X percentage)
+    tdEl = document.createElement('td')
+    tdEl.innerHTML = `${C1X_percentage} %`
+    trEl.appendChild(tdEl)
 
-    // Lager tbody element
-    let tbodyEl = document.createElement('tbody')
 
-    let madeTotal = 0
-    let triedTotal = 0
+    // Legger raden til tbody hvis den ikke er helt tom
+    if (!isNaN(C1X_percentage)) {
+      tbodyEl.appendChild(trEl)
+    }
 
-    // Går gjennom alle objektene i arrayet
-    // Dette er alle de ulike putterne
-    discs.forEach(disc => {
-        let trEl = document.createElement('tr')
+    madeTotal += made
+    triedTotal += tried
+  })
 
-        // Navnet på discen
-        let tdEl = document.createElement('td')
-        tdEl.innerHTML = disc.name
+  let C1X_percentage_total = (madeTotal / triedTotal) * 100
+  console.log(C1X_percentage_total)
 
-        // Legger kolonnen til raden
-        trEl.appendChild(tdEl)
+  C1X_percentage_total = Math.round(C1X_percentage_total)
 
-        let made = 0
-        let tried = 0
+  // Legger tbody til tabellen
+  tableEl.appendChild(tbodyEl)
 
-        // Lager kolonner
-        // Går gjennom alle distansene
-        distances.forEach(distance => {
-            // Putting stats
-            tdEl = document.createElement('td')
-            tdEl.innerHTML = `${disc[distance][0]} / ${disc[distance][1]}`
+  // Legger tabellen til div med id hoved
+  tableContainerEl.appendChild(tableEl)
 
-            made += disc[distance][0]
-            tried += disc[distance][1]
+  c1xEl.innerHTML = ''
+  c1xEl.innerHTML += `<h3 class='center'>Total C1X percentage: <br>${C1X_percentage_total} %</h3>`
 
-            // Legger kolonnen til raden
-            trEl.appendChild(tdEl)
-
-            C1X_data[distance][0] += disc[distance][0]
-            C1X_data[distance][1] += disc[distance][1]
-        })
-
-        let C1X_percentage = (made / tried) * 100
-        C1X_percentage = Math.round(C1X_percentage)
-
-        // Siste kolonne (C1X percentage)
-        tdEl = document.createElement('td')
-        tdEl.innerHTML = `${C1X_percentage} %`
-        trEl.appendChild(tdEl)
-
-        // Legger raden til tbody hvis den ikke er helt tom
-        if (!isNaN(C1X_percentage)) {
-            tbodyEl.appendChild(trEl)
-        }
-
-        madeTotal += made
-        triedTotal += tried
-    })
-
-    let C1X_percentage_total = (madeTotal / triedTotal) * 100
-    console.log(C1X_percentage_total)
-
-    C1X_percentage_total = Math.round(C1X_percentage_total)
-
-    // Legger tbody til tabellen
-    tableEl.appendChild(tbodyEl)
-
-    // Legger tabellen til div med id hoved
-    tableContainerEl.appendChild(tableEl)
-
-    c1xEl.innerHTML = ''
-    c1xEl.innerHTML += `<h3 class='center'>Total C1X percentage: <br>${C1X_percentage_total} %</h3>`
-
-    //console.log(C1X_data)
-    drawBarChart(makeData(C1X_data))
+  //console.log(C1X_data)
+  drawBarChart(makeData(C1X_data))
 }
 
 
 
 function makeData(dataObj) {
-    let dataArr = []
-    distances.forEach(distance => {
-        dataArr.push(100 * (dataObj[distance][0] / dataObj[distance][1]))
-    })
+  let dataArr = []
+  distances.forEach(distance => {
+    dataArr.push(100 * (dataObj[distance][0] / dataObj[distance][1]))
+  })
 
-    return dataArr
+  return dataArr
 }
 
 const canvasEl = document.querySelector('canvas')
@@ -197,23 +221,23 @@ let ctx = canvasEl.getContext('2d');
 let barChart = new Chart(ctx)
 
 // Function for drawing bar chart
-function drawBarChart(dataArr) {   
-    barChart.destroy();
-    barChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ["4 m", "5 m", "6 m", "7 m", "8 m", "9 m"],
-            datasets: [{
-                label: 'Putting percentage',
-                data: dataArr,
-            }]
-        },
-        scales: {
-            y: {
-                beginAtZero: true,
-            }
-        }
-    })
+function drawBarChart(dataArr) {
+  barChart.destroy();
+  barChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["4 m", "5 m", "6 m", "7 m", "8 m", "9 m"],
+      datasets: [{
+        label: 'Putting percentage',
+        data: dataArr,
+      }]
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+      }
+    }
+  })
 }
 
 //let dataArr = [95, 80, 70, 60, 40, 10]
