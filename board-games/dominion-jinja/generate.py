2f66e27e38866b2d117c20bb0926f25f43a95213
Didrik147 <didrik.roest@yahoo.com>
2026-08-08 21:51:22 +0200

    d

--- board-games/dominion-jinja/generate.py
+++ board-games/dominion-jinja/generate.py
@@ -8,18 +8,28 @@ env = Environment(loader=FileSystemLoader("."))
 template = env.get_template("template.html")
 
 
-df = pd.read_excel("dominion-cards.xlsx")
-df['img'] = './images/' + df['expansion'] + '/' + df['fixed_text'] + '.jpg'
-df.drop(columns=['url'], inplace=True)
+df = pd.read_excel("dominion-cards.xlsx", sheet_name="cards", engine="openpyxl")
+df['img'] = './images/' + df['expansion'] + '/' + df['expansion'] + '_' + df['fixed_text'] + '.jpg'
 
 cards = df.to_dict(orient="records")
 
-for card in cards:
-  print(card)
+df_set = pd.read_excel("dominion-cards.xlsx", sheet_name="paths_to_victory", engine="openpyxl")
+
+selected_names = (
+    df_set.melt()['value']
+    .dropna()
+    .astype(str)
+    .str.replace(" ", "")
+    .str.replace("'", "")
+    .str.lower()
+    .to_list()
+)
+
+selection = [card for card in cards if card['fixed_text'] in selected_names]
 
 
 # Render and write output
-html = template.render(cards=cards)
+html = template.render(cards=selection)
 with open("index.html", "w", encoding="utf-8") as f:
   f.write(html)
 
